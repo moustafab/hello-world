9f447e411c23e9809e83ac6e6c4f64b76ce4ed74
Moustafa Baiou <mbaiou@vibrenthealth.com>
2018-10-14 00:10:37 -0400

    introducing Sprints

--- hello_world.go
+++ hello_world.go
@@ -6,5 +6,6 @@ func main() {
 	fmt.Println("What is your name?")
 	var name string
 	fmt.Scanln(&name)
-	fmt.Printf("Hello, world. This is golang! Welcome %v! \n", name)
+	output := fmt.Sprintf("Hello, world. This is golang! Welcome %v!", name)
+	fmt.Println(output)
 }
